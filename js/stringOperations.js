0b90cf6a3e44b44fdf4456e5620231eaa585c767
florinda-centralsoft <florinda@centralsoft.io>
2026-01-07 16:59:46 +0100

    string operations

---
+++ js/stringOperations.js
@@ -0,0 +1,2 @@
+const str =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing Akademia jCoders elit. In leo arcu, ornare vel cursus a, hendrerit quis neque. Etiam interdum lectus ac nibh egestas gravida. Fusce eu pellentesque metus.";
