d3b18623d8622ea60e41de7daa03a3d73570a496
florinda-centralsoft <florinda@centralsoft.io>
2026-01-07 16:52:49 +0100

    loops

---
+++ js/loops.js
@@ -0,0 +1,40 @@
+
+
+// Given an array arr. The task is to find the largest element in the given array. 
+
+// Input: arr[] = [10, 20, 4]
+// Output: 20
+// Explanation: Among 10, 20 and 4, 20 is the largest. 
+// Input: arr[] = [20, 10, 20, 4, 100]
+// Output: 100
+
+
+
+
+
+// Given an array of integers, find the sum of its elements.
+
+// Input : arr[] = {1, 2, 3}
+// Output : 6
+// Explanation: 1 + 2 + 3 = 6
+// Input : arr[] = {15, 12, 13, 10}
+// Output : 50
+
+
+// Given an array arr. The task is to find the smallest element in the given array. 
+
+// Input: arr[] = [10, 20, 4]
+// Output: 4
+// Explanation: Among 10, 20 and 4, 4 is the smallest. 
+// Input: arr[] = [20, 10, 20, 4, 1]
+// Output: 1
+
+
+
+// Given an array arr[] of $n$ integers, calculate the average of all the elements.
+// Input: arr[] = [1, 2, 3, 4, 5]
+// Output: 3.0
+// Explanation: Sum is 1 + 2 + 3 + 4 + 5 = 15 / 5 = 3. 
+// Input: arr[] = [10, 20, 30]
+// Output: 20.0
+
