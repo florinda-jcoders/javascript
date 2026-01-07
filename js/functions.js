c6a19902dcb014366bd525d4d65de5b23c461994
florinda-centralsoft <florinda@centralsoft.io>
2026-01-07 16:30:17 +0100

    functions

---
+++ js/functions.js
@@ -0,0 +1,36 @@
+function hello() {
+  alert("hello there!");
+}
+
+// hello();
+// hello();
+// hello();
+
+// Konvertuesi i temperatures C => F
+// Formula: F = C * (9/5) + 32
+// Test cases: 0C = 32F, 100C = 212F, 32C = 89.6F
+
+
+// Konvertuesi i termperatures F => C
+// Formula: C = (F - 32)*(5/9)
+// Test cases: 32F = 0C, 100F = 37.7C, 54F = 12.2C
+
+
+// Kalkulatori i moshes se qeneve 
+// Human Age = 16 * ln(Dog's Age) + 31 // use Math.log() for the ln
+// Test cases: 1 = 31, 7 = 62.1, 12 = 70.7
+
+
+// Kontrollo nese nje numer eshte negative, positive ose 0
+
+
+// Kontrollo nese nje string eshte empty ose jo
+
+
+// Merr dy numra si argumente dhe gjej numrin me te madh
+
+
+// Kontrollo nese nje numer eshte i plotepjestueshem me 3 dhe 5 
+
+
+// Kontrollo nese numri i dhene eshte cift apo tek 
